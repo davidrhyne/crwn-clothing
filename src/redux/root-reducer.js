9322b38921d90d8476c53aa939d0a31b6878155c
DAVID J RHYNE <davidrhyne@att.net>
2020-11-27 08:03:30 -0600

    add redux-persist to app to store cart in localstorage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,12 +3,29 @@
 // they are split amoungst component and reducer combines them
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// implements window.localStorage
+import storage from 'redux-persist/lib/storage';
+
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['cart'] //what reducer(s) to store
+}
 
-export default combineReducers({
+// before redux-persist
+// export default combineReducers({
+//     user: userReducer,
+//     cart: cartReducer
+// })
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+//const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// implement redux persist
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
+
+//export default store;
+export default { store, persistor };
 
